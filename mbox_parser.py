ab79133c1d4ddbb093cd12b83122d3f4d779766e
gray-area <91284249+gray-area@users.noreply.github.com>
2025-04-10 10:15:51 -0400

    Update mbox_parser.py

--- mbox_parser.py
+++ mbox_parser.py
@@ -1,1 +1,19 @@
+import mailbox
+from email.utils import parsedate_to_datetime
 
+def parse_mbox(mbox_file):
+    """Parse the MBOX file and return a list of emails."""
+    emails = []
+    
+    mbox = mailbox.mbox(mbox_file)
+    for message in mbox:
+        email = {
+            "subject": message["subject"],
+            "from": message["from"],
+            "to": message["to"],
+            "date": parsedate_to_datetime(message["date"]),
+            "body": message.get_payload(decode=True),
+        }
+        emails.append(email)
+    
+    return emails
