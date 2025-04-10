a2d93d83050e342d2de4e449ed98c631a1c435dc
gray-area <91284249+gray-area@users.noreply.github.com>
2025-04-10 10:16:16 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+import os
+import logging
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+def log(message):
+    """Log a message to the console."""
+    logging.info(message)
+
+def check_pst_exists(pst_file):
+    """Check if the PST file exists."""
+    return os.path.exists(pst_file)
